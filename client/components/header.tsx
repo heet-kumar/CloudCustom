d67f67298387d8ac5e5181df68558faeb8428e5a
Kumar_ Heet (heekumar@deloitte.com) <heekumar@your.hostname.com>
2023-05-08 22:32:03 +0530

    Login Page Updated

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -2,7 +2,7 @@
 
 const Header:React.FC = () => {
     return(
-        <div className="fw-bolder p-3 fs-2 text-light" style={{backgroundColor: '#262f3d'}}>
+        <div className="fw-bolder w-100 p-3 fs-2 text-light position-fixed" style={{backgroundColor: '#262f3d'}}>
             HashedIn
             <span className="text-danger" style={{fontSize: '115%'}}>Cloud</span>
         </div>
--- client/components/login.tsx
+++ client/components/login.tsx
@@ -1,20 +1,24 @@
 import {FaUserCircle} from 'react-icons/fa';
 import {RiLockPasswordFill, RiLockPasswordLine} from 'react-icons/ri'
 import {MdOutlineAlternateEmail} from 'react-icons/md'
+import { useState } from 'react';
 
 
 const Login: React.FC = () => {
 
+    const [msg,setmsg] = useState<string>("");
+
     return(
         <div className="d-flex .justify-content-between">
-            <div className="login_left w-10">
+            <div className="login_left w-50">
                 <img src="/cloud-services.png" className="img-fluid" alt="cloud image" />
             </div>
             <div className="login_right w-50 d-flex justify-content-center align-items-center">
                 <div className='border border-dark p-5 d-flex flex-column gap-4 position-relative rounded'>
-                    <div className='position-absolute ' style={{top: '-12%'}}>
+                    <div className='position-absolute ' style={{top: '-11.5%'}}>
                         <FaUserCircle color={'#262f3d'} size={'70'}/>
                     </div>
+                    <div className='text-danger'>{msg}</div>
                     <div className="input-group input-group-lg ">
                         <span className="input-group-text" id="inputGroup-sizing-lg"><MdOutlineAlternateEmail size={'25'}/></span>
                         <input 
@@ -32,7 +36,7 @@ const Login: React.FC = () => {
                         />
                     </div>
                     <button type="button" className="btn btn-outline-dark">Login</button>
-                    <a className="btn btn-link link-offset-2 link-underline link-underline-opacity-0" role="button" >Create account</a>
+                    <a className="align-self-start btn btn-link link-offset-2 link-underline link-underline-opacity-0" role="button" >Create account</a>
                 </div>
             </div>
         </div>
