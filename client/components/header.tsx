184552422491d969f2e853935a5f0e554ce127da
Kumar_ Heet (heekumar@deloitte.com) <heekumar@your.hostname.com>
2023-05-12 13:45:07 +0530

    Search Bar Added

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,9 +1,42 @@
+import { Autocomplete, TextField } from "@mui/material";
+import axios from "axios";
 import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
+interface ServiceData {
+    sid: number,
+    name: string,
+    desc: string
+}
 
 const Header:React.FC = () => {
 
     const router = useRouter();
+
+    const [data,setdata] = useState<Array<ServiceData>>([]);
+    const [searchText,setsearch] = useState<string>();
+
+    useEffect( () => {
+
+        const getData = async() => {
+            await axios.get("http://localhost:5000/services/all")
+            .then( res => {
+                console.log(res.data.msg)
+                setdata(res.data.msg);
+            })
+            .catch( err => {
+                console.log(err);
+            })
+        }
+        if(router.asPath === '/dashboard') getData();
+
+    },[])
+
+    const handleSearch = (e:React.ChangeEvent<HTMLInputElement>,newValue:any) => {
+        setsearch(newValue);
+        console.log(searchText)
+    }
+
 
     return(
         <div className="d-flex justify-content-between w-100 p-3 position-fixed shadow-lg" style={{backgroundColor: '#262f3d', zIndex: '12'}}>
@@ -13,6 +46,34 @@ const Header:React.FC = () => {
             </div>
             {
                 (router.asPath !== '/' && router.asPath !== '/signup')?
+
+                <div className="d-flex justify-content-between w-50">
+                    <Autocomplete
+                        id="free-solo-demo"
+                        style={{width:'70%'}}
+                        sx={{
+                            "& input": {
+                                color: 'black',
+                                backgroundColor: '#fff'
+                            },
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                  borderColor: 'white',
+                                  color: 'black'
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: 'white',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: 'white',
+                                },
+                            }
+                        }}
+                        freeSolo
+                        options={data.map((option) => option.name)}
+                        onChange={handleSearch}
+                        renderInput={(params) => <TextField {...params} label="freeSolo" />}
+                    />
                     <div className="align-self-center">
                         <button 
                             type="button" 
@@ -24,7 +85,8 @@ const Header:React.FC = () => {
                         >
                             Logout
                         </button>
-                    </div>:<></>
+                    </div>
+                </div>:<></>
             }
         </div>
     );
